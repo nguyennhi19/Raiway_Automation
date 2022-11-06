53b385e9dc767e9b4c35e2b1ca52648ea80556fc
nguyennhi19 <83210883+nguyennhi19@users.noreply.github.com>
2022-11-06 12:57:11 +0700

    Update TicketPricePage.java

--- src/main/java/java/com/TicketPricePage.java
+++ src/main/java/java/com/TicketPricePage.java
@@ -7,9 +7,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TicketPricePage {
     //Locator
-    //td[text()='Hard seat']//following-sibling::td//a           get seatype from ticketprice
-    //th[contains(text(),'Price')]//following-sibling::td[1]     get price from ticketprice
-
+  
     private By btnCheckPrice = By.xpath("//li[text()='%s to %s']/parent::td//following-sibling::td//a[text()='Check Price']");
 
     //Elements
@@ -19,13 +17,6 @@ public class TicketPricePage {
         return driver.findElement(btnCheckPrice);
     }
 
-    public String getTrain(String departFrom, String arriveAt){
-        String text = "//li[text()='"+departFrom+" to "+arriveAt+"']/parent::td//following-sibling::td//a[text()='Check Price']";
-        return text;
-    }
-
     //Methods
-    public void checkPriceFromTicketPricePage(String departFrom, String arriveAt){
-        driver.findElement(By.xpath(getTrain(departFrom,arriveAt))).click();
-    }
+    
 }
