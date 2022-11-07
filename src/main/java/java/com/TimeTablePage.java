a74f9128198b61abe98d74e2b3ede5a59a71d9e4
nguyennhi19 <83210883+nguyennhi19@users.noreply.github.com>
2022-11-07 08:37:29 +0700

    Update TimeTablePage.java

--- src/main/java/java/com/TimeTablePage.java
+++ src/main/java/java/com/TimeTablePage.java
@@ -7,10 +7,18 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TimeTablePage {
     // Locators
-
+    private By btnCheckPrice = By.xpath("//td[text()='%s']//following-sibling::td[text()='%s']//following-sibling::td//a[text()='check price']");
+    private By btnBookTicket = By.xpath("//td[text()='%s']//following-sibling::td[text()='%s']//following-sibling::td//a[text()='book ticket']");
     //Elements
+    WebDriver driver = new ChromeDriver();
+
+    private WebElement getBtnCheckPrice(){
+        return driver.findElement(btnCheckPrice);
+    }
+    private WebElement getBtnBookTicket(){
+        return driver.findElement(btnBookTicket);
+    }
 
     //Methods
-    WebDriver driver = new ChromeDriver();
     
 }
