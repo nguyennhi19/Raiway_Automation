aa84359fbcda061dd7bea919fc40e517f9f78950
nguyennhi19 <83210883+nguyennhi19@users.noreply.github.com>
2022-11-07 08:36:50 +0700

    Update TicketPricePage.java

--- src/main/java/java/com/TicketPricePage.java
+++ src/main/java/java/com/TicketPricePage.java
@@ -7,14 +7,23 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TicketPricePage {
     //Locator
-  
     private By btnCheckPrice = By.xpath("//li[text()='%s to %s']/parent::td//following-sibling::td//a[text()='Check Price']");
+    private By btnBookTicket = By.xpath("//td[text()='%s']//following-sibling::td//a");
+    private By PriceOfEachSeat= By.xpath("//th[contains(text(),'Price')]//following-sibling::td[%s]");
 
     //Elements
     WebDriver driver = new ChromeDriver();
 
     private WebElement getBtnCheckPrice(){
         return driver.findElement(btnCheckPrice);
+    }
+
+    private WebElement getBtnBookTicket(){
+        return driver.findElement(btnBookTicket);
+    }
+
+    private WebElement gettextPriceOfEachSeat(){
+        return driver.findElement(PriceOfEachSeat);
     }
 
     //Methods
