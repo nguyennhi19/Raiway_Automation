682a65fd7459a30c734848ccdacc808b2a9d82d6
nguyennhi19 <83210883+nguyennhi19@users.noreply.github.com>
2022-11-07 09:31:56 +0700

    Update GeneralPage.java

--- src/main/java/java/com/GeneralPage.java
+++ src/main/java/java/com/GeneralPage.java
@@ -8,53 +8,53 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class GeneralPage {
     //Locators
-    private final By tabLogin = By.linkText("Login");
-    private final By tabLogout = By.linkText("Log out");
-    private final By tabBookTicket = By.linkText("Book ticket");
-    private final By tabContact = By.linkText("Contact");
-    private final By tabRegister = By.linkText("Register");
-    private final By tabChangePassword = By.linkText("Change password");
-    private final By tabTimeTable = By.linkText("Timetable");
-    private final By tabMyTicket = By.linkText("My ticket");
-    private final By lblWelcomeUser = By.xpath("//div[@class='account']//strong");
+    private By tabLogin = By.linkText("Login");
+    private By tabLogout = By.linkText("Log out");
+    private By tabBookTicket = By.linkText("Book ticket");
+    private By tabContact = By.linkText("Contact");
+    private By tabRegister = By.linkText("Register");
+    private By tabChangePassword = By.linkText("Change password");
+    private By tabTimeTable = By.linkText("Timetable");
+    private By tabMyTicket = By.linkText("My ticket");
+    private By lblWelcomeUser = By.xpath("//div[@class='account']//strong");
 
     //Elements
 
     WebDriver driver = new ChromeDriver();
 
-    protected WebElement getTabLogin() {
+    private WebElement getTabLogin() {
         return driver.findElement(tabLogin);
     }
 
-    protected WebElement getTabLogout() {
+    private WebElement getTabLogout() {
         return driver.findElement(tabLogout);
     }
 
-    protected WebElement getTabBookTicket() {
+    private WebElement getTabBookTicket() {
         return driver.findElement(tabBookTicket);
     }
 
-    protected WebElement getTabContact() {
+    private WebElement getTabContact() {
         return driver.findElement(tabContact);
     }
 
-    protected WebElement getTabRegister() {
+    private WebElement getTabRegister() {
         return driver.findElement(tabRegister);
     }
 
-    protected WebElement getTabChangePassword() {
+    private WebElement getTabChangePassword() {
         return driver.findElement(tabChangePassword);
     }
 
-    protected WebElement getTabTimetable() {
+    private WebElement getTabTimetable() {
         return driver.findElement(tabTimeTable);
     }
 
-    protected WebElement getTabMyTicket() {
+    private WebElement getTabMyTicket() {
         return driver.findElement(tabMyTicket);
     }
 
-    protected WebElement getWelcomeUserElement() {
+    private WebElement getWelcomeUserElement() {
         return driver.findElement(lblWelcomeUser);
     }
 
