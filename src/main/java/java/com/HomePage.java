fc489ab640f0a65d94ebe9eb06ffab252ed823de
nguyennhi19 <83210883+nguyennhi19@users.noreply.github.com>
2022-11-07 09:30:26 +0700

    Update HomePage.java

--- src/main/java/java/com/HomePage.java
+++ src/main/java/java/com/HomePage.java
@@ -4,10 +4,10 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class HomePage {
-    public static final String RAILWAY_URL = "http://www.railwayb2.somee.com/Page/HomePage.cshtml";
+    
 
     public void open() {
         WebDriver driver = new ChromeDriver();
-        driver.navigate().to(RAILWAY_URL);
+        driver.navigate().to("http://www.railwayb2.somee.com/Page/HomePage.cshtml");
     }
 }
